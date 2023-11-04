f013087e442eb47ee106ceab8086c7bdddba382c
aryunin <im.aryunin@gmail.com>
2023-11-04 18:37:45 +0300

    async applied

--- flutter_application_1/lib/pages/first_page.dart
+++ flutter_application_1/lib/pages/first_page.dart
@@ -1,44 +1,42 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/post.dart';
 import 'package:flutter_application_1/services/remote_serveices.dart';
 
-
-class first_page extends StatefulWidget{
-  first_page ({super.key});
+class first_page extends StatefulWidget {
+  first_page({super.key});
 
   @override
   State<first_page> createState() => _first_page();
-
 }
 
-class _first_page extends State<first_page>{
+class _first_page extends State<first_page> {
+  List<Posts>? post = [];
 
-  List<Posts>? post =[];
-  
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getData();
   }
 
   getData() async {
-    post = (await RemoteService().getPosts());
-    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
-    
-    }
-    
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(title: const Text('Рейтинг привлекательности регионов')),
-          body: post == null || post!.isEmpty 
-            ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: post!.length,
-              itemBuilder: (context, index){
-                Text(post![index].name);
-              })
-        );
-      } 
+    RemoteService().getPosts().then((value) {
+      post = value;
+      super.setState(() {});
+    });
   }
-  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Рейтинг привлекательности регионов')),
+        body: post == null || post!.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: post!.length,
+                itemBuilder: (context, index) {
+                  Text(post![index].name);
+                }));
+  }
+}
--- flutter_application_1/lib/services/remote_serveices.dart
+++ flutter_application_1/lib/services/remote_serveices.dart
@@ -4,23 +4,23 @@ import 'package:flutter_application_1/models/post.dart';
 import 'package:http/http.dart' as http;
 
 class RemoteService {
-  Future<List<Posts>?> getPosts () async{
-    
-    try{
-    var uri = Uri.parse("http://localhost:8080/api/regions/users");
-    var response = await http.get(uri);
-    if (response.statusCode == 200){
-      var json = response.body;
-      List<Posts> _model = postsFromJson(response.body);
-      return _model;
-    } 
-    } catch (e){
+  Future<List<Posts>?> getPosts() async {
+    try {
+      var uri = Uri.parse("http://10.0.2.2:8080/api/regions");
+      var response =
+          await Future.delayed(Duration(seconds: 5), () => http.get(uri));
+      if (response.statusCode == 200) {
+        var json = response.body;
+        List<Posts> _model = postsFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
       log(e.toString());
     }
   }
 }
 
-class ApiConstants{
-   static String baseUrl = 'http://localhost:8080/api/regions';
-   static String usersEndPoint = '/users'; 
+class ApiConstants {
+  static String baseUrl = 'http://10.0.2.2:8080/api/regions';
+  static String usersEndPoint = '/users';
 }
