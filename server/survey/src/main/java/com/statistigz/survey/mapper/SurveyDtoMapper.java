b7dda5a1c38534ba18f4c1f301ca26897c3898f7
Артем <37240301+aryunin@users.noreply.github.com>
2024-05-01 15:16:51 +0300

    ticket 96 (#97)

--- server/survey/src/main/java/com/statistigz/survey/mapper/SurveyDtoMapper.java
+++ server/survey/src/main/java/com/statistigz/survey/mapper/SurveyDtoMapper.java
@@ -2,50 +2,55 @@ package com.statistigz.survey.mapper;
 
 import com.statistigz.common.dto.survey.QuestionDTO;
 import com.statistigz.common.dto.survey.SurveyDTO;
-import com.statistigz.common.util.Mapper;
 import com.statistigz.survey.entity.Question;
 import com.statistigz.survey.entity.Survey;
 import com.statistigz.survey.util.CustomLogger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-// TODO reactive mapper ?
-
 @Component
 @RequiredArgsConstructor
-public final class SurveyDtoMapper implements Mapper<Survey, SurveyDTO> {
+public final class SurveyDtoMapper implements MonoMapper<Survey, SurveyDTO> {
     private final CustomLogger logger;
-    private final Mapper<Question, QuestionDTO> questionDtoMapper;
+    private final MonoMapper<Question, QuestionDTO> questionDtoMapper;
 
     @Override
-    public SurveyDTO mapTo(Survey survey) {
+    public Mono<SurveyDTO> mapTo(Survey survey) {
         logger.debug(this, "mapTo()");
         logger.debug(this, "survey = " + survey);
 
-        List<QuestionDTO> questions = survey.getQuestions()
-                .stream()
-                .map(questionDtoMapper::mapTo)
-                .toList();
+        Flux<QuestionDTO> questions = Flux.fromIterable(survey.getQuestions())
+                .flatMapSequential(questionDtoMapper::mapTo);
 
-        logger.debug(this, "questionDTOs = " + questions);
-
-        return new SurveyDTO(survey.getId(), survey.getName(), questions);
+        return Mono.just(survey)
+                .zipWith(questions.collectList())
+                .map(tuple -> {
+                    Survey t1 = tuple.getT1();
+                    List<QuestionDTO> t2 = tuple.getT2();
+                    logger.debug(this, "questionsDTOs = " + t2);
+                    return new SurveyDTO(t1.getId(), t1.getName(), t2);
+                });
     }
 
     @Override
-    public Survey mapFrom(SurveyDTO surveyDto) {
+    public Mono<Survey> mapFrom(SurveyDTO surveyDto) {
         logger.debug(this, "mapFrom()");
         logger.debug(this, "surveyDto = " + surveyDto);
 
-        List<Question> questions = surveyDto.questions()
-                .stream()
-                .map(questionDtoMapper::mapFrom)
-                .toList();
+        Flux<Question> questions = Flux.fromIterable(surveyDto.questions())
+                .flatMapSequential(questionDtoMapper::mapFrom);
 
-        logger.debug(this, "questions = " + questions);
-
-        return new Survey(surveyDto.id(), surveyDto.name(), questions);
+        return Mono.just(surveyDto)
+                .zipWith(questions.collectList())
+                .map(tuple -> {
+                    SurveyDTO t1 = tuple.getT1();
+                    List<Question> t2 = tuple.getT2();
+                    logger.debug(this, "questions = " + t2);
+                    return new Survey(t1.id(), t1.name(), t2);
+                });
     }
 }
