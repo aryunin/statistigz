7fbfa08e82b8358aa967ca01dea92097ae4b998c
Artem Yunin <im.aryunin@gmail.com>
2024-11-21 22:03:13 +0300

    test fix

--- server/cp/src/test/java/com/statistigz/cp/CpApplicationTests.java
+++ server/cp/src/test/java/com/statistigz/cp/CpApplicationTests.java
@@ -1,10 +1,14 @@
 package com.statistigz.cp;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class CpApplicationTests {
+	@MockBean
+	private EntityManager entityManager;
 
 	@Test
 	void contextLoads() {
