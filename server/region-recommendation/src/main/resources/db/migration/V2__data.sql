ea08ed78cf7ec75bf6e3d3649fbc932670052eb2
Артем <37240301+aryunin@users.noreply.github.com>
2024-12-01 17:05:23 +0300

    ticket 134 (#137)

--- server/region-recommendation/src/main/resources/db/migration/V2__data.sql
+++ server/region-recommendation/src/main/resources/db/migration/V2__data.sql
@@ -1,3 +1,17 @@
--- Для тестов
+INSERT INTO cluster(id) VALUES (0);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (0, 7), (0, 19), (0, 21), (0, 51), (0, 72);
+
 INSERT INTO cluster(id) VALUES (1);
-INSERT INTO cluster_region(cluster_id, region_id) VALUES (1, 1), (1, 2), (1, 3);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (1, 1), (1, 6), (1, 47), (1, 52), (1, 60);
+
+INSERT INTO cluster(id) VALUES (2);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (2, 1), (2, 18), (2, 28), (2, 47), (2, 60);
+
+INSERT INTO cluster(id) VALUES (3);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (3, 10), (3, 16), (3, 18), (3, 28), (3, 55);
+
+INSERT INTO cluster(id) VALUES (4);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (4, 18), (4, 28), (4, 47), (4, 60), (4, 80);
+
+INSERT INTO cluster(id) VALUES (5);
+INSERT INTO cluster_region(cluster_id, region_id) VALUES (5, 73), (5, 74), (5, 77), (5, 79), (5, 80);
