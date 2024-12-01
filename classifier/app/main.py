f1d623fededf53ad90cc5fa9be5eec8371f67648
agoihpeg <110656939+agoihpeg@users.noreply.github.com>
2024-12-01 11:59:26 +0300

    Classifier implemented

--- classifier/app/main.py
+++ classifier/app/main.py
@@ -14,14 +14,25 @@ class RequestDTO(BaseModel):
 class ClusterResponseDTO(BaseModel):
     cluster: int
 
-model = joblib.load('model.sav')
+model = joblib.load('kmeans.sav')
+pca = joblib.load('pca.sav')
+slice = np.array([5,7,8,9,10,12,13,14,15,16,17]) - 1
+maximums = np.array([4,4,4,4,4,4,4,4,4,4,2], dtype=np.float64)
 
 def predict(answers: List[int]):
     global model
-    model.predict([np.array(answers)])
+    global pca
+    global slice
+    global maximums
+    answers = np.array(answers, dtype=np.float64)[slice]
+    answers /= maximums
+    answers = pca.transform((answers,))
+    return model.predict(answers)[0]
 
 
 
 @app.post("/")
-async def root(data: RequestDTO): 
-    return ClusterResponseDTO(cluster=predict(data.answers))
+async def root(data: RequestDTO):
+    cluster = predict(data.answers)
+    response = ClusterResponseDTO(cluster=cluster)
+    return response
