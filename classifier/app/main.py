4ccc383a0443fa8305595dbfe906ee84f29c177b
Артем <37240301+aryunin@users.noreply.github.com>
2024-12-01 16:47:51 +0300

    ticket 67 (#135)
    Co-authored-by: agoihpeg <110656939+agoihpeg@users.noreply.github.com>

--- classifier/app/main.py
+++ classifier/app/main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from typing import List
 import uvicorn
 import os
+import joblib
+import numpy as np
 
 app = FastAPI()
 
@@ -12,6 +14,25 @@ class RequestDTO(BaseModel):
 class ClusterResponseDTO(BaseModel):
     cluster: int
 
+model = joblib.load('kmeans.sav')
+pca = joblib.load('pca.sav')
+slice = np.array([5,7,8,9,10,12,13,14,15,16,17]) - 1
+maximums = np.array([4,4,4,4,4,4,4,4,4,4,2], dtype=np.float64)
+
+def predict(answers: List[int]):
+    global model
+    global pca
+    global slice
+    global maximums
+    answers = np.array(answers, dtype=np.float64)[slice]
+    answers /= maximums
+    answers = pca.transform((answers,))
+    return model.predict(answers)[0]
+
+
+
 @app.post("/")
-async def root(data: RequestDTO): 
-    return ClusterResponseDTO(cluster=1)
+async def root(data: RequestDTO):
+    cluster = predict(data.answers)
+    response = ClusterResponseDTO(cluster=cluster)
+    return response
